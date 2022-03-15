5a87e40c110b6f1e8ef5447f424066ee90bfb097
Gabriel Martinez <gabirio86@gmail.com>
2022-03-14 23:55:42 -0300

    First part of final project

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ function App() {
           <Route path='/' element={<ItemListContainer/>} />
           <Route path='/category/:categoryId' element={<ItemListContainer/>} />
 
+          <Route path='/*' element={ <Navigate to='/' replace /> } />
         </Routes>
       </BrowserRouter>
 
