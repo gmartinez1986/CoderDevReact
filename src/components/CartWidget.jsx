6b7b815d850851b1452c6105b44d105251e8b1a6
Gabriel Martinez <gabirio86@gmail.com>
2022-03-24 15:25:00 -0300

    Cart View

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useCartContext } from "../context/CartContext"
 
 const CartWidget = () => {
+
+    const { quantityItem } = useCartContext();
+
+    let cssClass = {};
+    let count = quantityItem();
+
+    if(count > 0){
+        cssClass = {className: 'badge on'};
+    }
+
     return (
 
         <li className="nav-item">
             <Link to="/cart" >
                 <a className="nav-link" href="">
                     <span className='material-icons'>shopping_cart</span>
-                    <span id="count"></span>
+                    <span {...cssClass}>{count > 0 ? count:""}</span>
                 </a>
             </Link>
         </li>
