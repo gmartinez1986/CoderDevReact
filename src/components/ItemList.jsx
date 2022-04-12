d3dafed37f9b7e92f0cec686c404ee2e85ee9e5d
Gabriel Martinez <gabirio86@gmail.com>
2022-04-12 16:26:55 -0300

    Final Project

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,8 +3,20 @@ import { useParams } from "react-router-dom";
 import ItemCard from "./ItemCard";
 import Item from "./Item";
 
+
 const ItemList = memo(
     ({ packages }) => {
+
+        //Ordeno el listado de paquetes turisticos a partir del campo orden.
+        packages.sort(function (a, b) {
+            if (a.order > b.order) {
+              return 1;
+            }
+            if (a.order < b.order) {
+              return -1;
+            }
+            return 0;
+        });
 
         const { categoryId } = useParams();
 
